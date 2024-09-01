4b5632b2af40f424eb1b0bf73191064b572ef2eb
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2024-09-01 13:13:16 +0530

    Add files via upload

---
+++ Arduino_Motor_Shiled_With_Blutooth/Arduino_Motor_Shiled_With_Blutooth.ino
@@ -0,0 +1,98 @@
+
+#include <SoftwareSerial.h>
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ);
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+SoftwareSerial bt(2, 3);
+char data;
+
+void setup() {
+  Serial.begin(9600);
+  bt.begin(9600);
+}
+
+void loop() {
+  while (bt.available()) {
+    {
+      data = bt.read();
+      Serial.println(data);
+      Stop();
+    }
+    switch (data) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+    }
+  }
+  if (bt.available() < 0) {
+  }
+  delay(100);
+}
+
+void forward() {
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+
+void back() {
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(255);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(255);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(255);
+  motor4.run(BACKWARD);
+}
+
+void left() {
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(255);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+
+void right() {
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(255);
+  motor4.run(BACKWARD);
+}
+
+void Stop() {
+  motor1.setSpeed(0);
+  motor1.run(RELEASE);
+  motor2.setSpeed(0);
+  motor2.run(RELEASE);
+  motor3.setSpeed(0);
+  motor3.run(RELEASE);
+  motor4.setSpeed(0);
+  motor4.run(RELEASE);
+}
